6ffff1290f37e1b650e0b395680272ce16fddf9d
Vladimir Vorobiev <30821255+noatatat@users.noreply.github.com>
2019-02-15 09:46:02 +0300

    Create events.ts

---
+++ src/app/events.ts
@@ -0,0 +1,44 @@
+export const events = {
+    headers: [
+      { field: 'date', header: 'Date' },
+      { field: 'kind', header: 'Kind' },
+      { field: 'code', header: 'Code' },
+    ],
+    data: [
+      {
+        date: '20.04.2018',
+        kind: 'Сreate obstacles',
+        id: '123071209839011',
+        code: 'JOIR234260746102374607213647279',
+        subEvents: [
+          { id: "12312312", code: 'PAKJPASH092830940923848203' },
+          { id: "12312312", code: 'HKJHAKFHA28409831098409218' },
+          { id: "12312312", code: 'LLSPOKSD382984709283748732' },
+          { id: "12312312", code: 'FJLDSKJF827342398472938478' },
+        ]
+      },
+      {
+        date: '21.04.2018',
+        kind: 'Juggle with notions',
+        id: '123071209839012',
+        code: 'BOLASERUH260746102374607213647279',
+        subEvents: [
+          { id: "12312312", code: 'PAKJPASH092830940923848203' },
+          { id: "12312312", code: 'HKJHAKFHA28409831098409218' },
+          { id: "12312312", code: 'LLSPOKSD382984709283748732' },
+          { id: "12312312", code: 'FJLDSKJF827342398472938478' },
+        ]
+      },{
+        date: '22.04.2018',
+        kind: 'trimming poodles',
+        id: '123071209839013',
+        code: 'MLKEDM61276481260394837292134',
+        subEvents: [
+          { id: "12312312", code: 'PAKJPASH092830940923848203' },
+          { id: "12312312", code: 'HKJHAKFHA28409831098409218' },
+          { id: "12312312", code: 'LLSPOKSD382984709283748732' },
+          { id: "12312312", code: 'FJLDSKJF827342398472938478' },
+        ]
+      },
+    ]
+  };
